4591fa827eb2dec7c8d3b2bc98b56212f9a0f665
iavn666 <dvoeglazovcom@gmail.com>
2021-08-16 20:29:04 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+
+    int status;
+
+    String message;
+
+      public Error() {
+
+      }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message = message;
+        this.status = status;
+    }
+
+    public void info() {
+        System.out.println("Activ: " + active);
+        System.out.println("Message: " + message);
+        System.out.println("Status: " + status);
+
+    }
+
+    public static void main(String[] args) {
+        Error one = new Error(false, 1, "tools");
+        one.info();
+        Error two = new Error();
+        two.info();
+    }
+
+}
