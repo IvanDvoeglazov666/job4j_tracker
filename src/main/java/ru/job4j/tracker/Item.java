56167fdb499cb93ec959206095da05cbd49ebb04
iavn666 <dvoeglazovcom@gmail.com>
2021-08-17 19:14:55 +0500

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,15 +1,23 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime currentDateTime = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    private String currentDateTimeFormat = currentDateTime.format(formatter);
+
 
     public Item() {
     }
 
-    public Item(int id, String name) {
+    public Item(int id, String name, String currentDateTimeFormat) {
         this.id = id;
         this.name = name;
+        this.currentDateTimeFormat = currentDateTimeFormat;
     }
 
     public Item(String name) {
@@ -31,4 +39,8 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+   public String getCurrentDateTimeFormat() {
+        return currentDateTimeFormat;
+   }
 }
