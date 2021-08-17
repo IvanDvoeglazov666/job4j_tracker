7f8aaf9d45366377eb546b264fc64e67f972e875
iavn666 <dvoeglazovcom@gmail.com>
2021-08-17 20:47:25 +0500

    2.2. Рефакторинг - Площадь треугольника. [#122642 #190960]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -14,15 +14,11 @@ public class Triangle {
     }
 
     public double semiPerimeter(double a, double b, double c) {
-        return a + b + c;
+        return (a + b + c)/2;
     }
 
     public boolean exist(double ab, double ac, double bc) {
-        boolean rsl = false;
-        if ((ab + ac) > bc && (ab + bc) > ac && (ac + bc) > ab) {
-            rsl = true;
-        }
-        return rsl;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 
     public double area() {
