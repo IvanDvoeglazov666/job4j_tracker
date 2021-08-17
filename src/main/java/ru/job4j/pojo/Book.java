710d053b0893de8c75b77cc047e137d49cdf450c
iavn666 <dvoeglazovcom@gmail.com>
2021-08-17 22:29:09 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,25 @@
+package ru.job4j.pojo;
+
+public class Book {
+
+    private String name;
+    private int page;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+}
+
+
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,30 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book one = new Book();
+        Book two = new Book();
+        Book three = new Book();
+        Book four = new Book();
+        Book[] bibliotek = new Book[4];
+        bibliotek[0] = one;
+        bibliotek[1] = two;
+        bibliotek[2] = three;
+        bibliotek[3] = four;
+        one.setName("Clean code");
+        for(int index = 0; index < bibliotek.length; index++) {
+            Book g = bibliotek[index];
+                if (index == 0) {
+                Book temp = bibliotek[index];
+                bibliotek[index] = bibliotek[bibliotek.length - 1];
+                bibliotek[index] = bibliotek[index + 1];
+                bibliotek[bibliotek.length - 1] = temp;
+            }
+              System.out.println(bibliotek[index].getName() + " " + bibliotek[index].getPage());
+
+
+
+        }
+
+    }
+}
