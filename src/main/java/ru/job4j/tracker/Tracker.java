4301c751994f26842b5cd0070194c4428c57e261
iavn666 <dvoeglazovcom@gmail.com>
2021-08-19 12:10:12 +0500

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,6 +56,19 @@ public class Tracker {
           return rsl;
     }
 
+    public boolean delete(int id) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = null;
+            System.arraycopy(items, index + 1, items, index, size);
+            items[size -1] = null;
+            size--;
+            rsl = true;
+        }
+       return rsl;
+    }
+
 
 
 
