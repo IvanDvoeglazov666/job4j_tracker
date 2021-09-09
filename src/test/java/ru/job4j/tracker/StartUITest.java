459492dbcb0cfe625e9db88a76f91f6d050b1431
iavn666 <dvoeglazovcom@gmail.com>
2021-09-09 20:04:05 +0500

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -35,9 +37,9 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        assertThat(tracker.findById(item.getId()), is(null));
+        int id = item.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(item.getId()), is(nullValue()));
 
     }
 }
