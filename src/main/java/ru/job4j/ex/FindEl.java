08a67db50a78d7b9f125c90da39c7f4cbe55e540
iavn666 <dvoeglazovcom@gmail.com>
2021-09-10 13:58:10 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws UserInputException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+           if(value[i].equals(key)) {
+               rsl = i;
+           }
+        }
+        if (rsl == -1) {
+            throw new UserInputException("no data returned");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] num = {"vanya", "petya", "vasya"};
+        String key = "petya";
+        try {
+            System.out.println(FindEl.indexOf(num,key));
+        } catch (UserInputException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
