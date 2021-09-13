963504b85f4ec9795f9df1c5165adaeab291c3e7
iavn666 <dvoeglazovcom@gmail.com>
2021-09-13 16:01:24 +0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String texts: origin) {
+            check.add(texts);
+        }
+        for (String texts: text) {
+            if(!check.contains(texts)){
+                 rsl = false;
+            }
+
+        }
+        /* for-each origin -> new HashSet. */
+        /* for-each text -> hashSet.contains */
+        return rsl;
+    }
+}
