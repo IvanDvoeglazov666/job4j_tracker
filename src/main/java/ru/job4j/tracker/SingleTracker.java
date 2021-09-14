317a5b1f9667b3d798a0cd9edeb9623aa78a18ff
iavn666 <dvoeglazovcom@gmail.com>
2021-09-14 12:08:53 +0500

    2. Singleton [#94606 #202236]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,38 +1,77 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class SingleTracker {
+public final class SingleTracker {
     private Tracker tracker = new Tracker();
-
-
+    private List<Item> items = new ArrayList<>();
+    private static SingleTracker instance = null;
 
     public Item add(Item item) {
         return tracker.add(item);
     }
 
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
     public Item findById(int id) {
-        return null;
+        int index = indexOf(id);
+        return index != -1 ? items.get(index) : null;
     }
 
-    public Item[] findAll() {
-        return null;
+    public List<Item> findAll() {
+        return List.copyOf(items);
     }
 
-    public Item[] findByName(String key) {
-        return null;
-    }
-
-    public boolean replace(int id, Item item) {
-        return true;
-    }
-
-    public boolean delete(int id) {
-        return true;
+    public List<Item> findByName(String key) {
+        List<Item> res = new ArrayList<>();
+        for (Item item: items) {
+            if(item.getName().equals(key)) {
+                res.add(item);
+            }
+        }
+        return res;
     }
 
     private int indexOf(int id) {
-        return 0;
+        int rsl = -1;
+        for (int index = 0; index < items.size(); index++) {
+            if (items.get(index).getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean result = index != -1;
+        if (result) {
+            item.setId(id);
+            items.set(index, item);
+        }
+        return result;
+    }
+
+    public boolean delete(int id) {
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items.remove(index);
+            rsl = true;
+        }
+        return rsl;
     }
 
 }
