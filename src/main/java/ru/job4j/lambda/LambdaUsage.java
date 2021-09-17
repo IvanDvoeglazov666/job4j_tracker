a5baf2757dd1529cee5fdc51f7a620c025f93c6f
iavn666 <dvoeglazovcom@gmail.com>
2021-09-17 14:21:19 +0500

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String[] arrays = {"Hello", "Hi", "Go", "Mid"};
+        Comparator<String> printInfo = (left, right) -> {
+          System.out.println("START: " + right.length() + " " + left.length() + " " + "FAINL");
+          return right.compareTo(left);
+        };
+        Arrays.sort(arrays, printInfo);
+    }
+}
