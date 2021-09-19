2b6e2633415abfa14c77747aaffd8ebb4050630f
iavn666 <dvoeglazovcom@gmail.com>
2021-09-19 08:08:13 +0500

    5. Подсчет функции в диапазоне. [#24251 #204593]

--- src/main/java/ru/job4j/lambda/Fun.java
+++ src/main/java/ru/job4j/lambda/Fun.java
@@ -8,9 +8,8 @@ public class Fun {
 
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> res = new ArrayList<>();
-        Function<Double, Double> er = (x) -> func.apply(x);
         for (int index = start; index < end; index++) {
-            res.add(er.apply((double) index));
+            res.add(func.apply((double) index));
         }
         return res;
     }
--- src/test/java/ru/job4j/lambda/FunTest.java
+++ src/test/java/ru/job4j/lambda/FunTest.java
@@ -28,8 +28,8 @@ public class FunTest {
     @Test
     public void whenLinearFunctionThenIndicativeResults() {
         Fun function = new Fun();
-        List<Double> result = function.diapason(5, 8, x -> Math.pow(x,x));
-        List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(2,x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
         assertThat(result, is(expected));
     }
 }
